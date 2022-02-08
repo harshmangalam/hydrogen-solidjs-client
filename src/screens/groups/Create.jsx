60dce1d0f00af1c31b423e4bc54799ab147396cf
Harsh Mangalam <hmangalam@oplglobal.com>
2022-02-08 17:27:28 +0530

    dark create group

--- src/screens/groups/Create.jsx
+++ src/screens/groups/Create.jsx
@@ -1,19 +1,34 @@
+import { FaSolidUsers } from "solid-icons/fa";
 export default function Create() {
   function handleCreateGroup() {}
   return (
-    <div className="">
-      <section className="max-w-md mx-auto bg-white  px-4 py-4  rounded-lg">
+    <div className="py-10">
+      <div className="my-4 flex flex-col items-center space-y-2 max-w-md mx-auto">
+       <div className="w-16 h-16 bg-blue-500 rounded-full grid place-items-center">
+       <FaSolidUsers className="text-4xl text-white" />
+       </div>
+        <h5 className="text-2xl font-medium ">Create Group</h5>
+      </div>
+      <section className="max-w-md mx-auto bg-white dark:bg-gray-800  px-4 py-8  rounded-lg shadow">
         <form
           onSubmit={[handleCreateGroup]}
           className="flex flex-col space-y-4"
         >
           <div className="flex flex-col space-y-2">
             <label htmlFor="name">Group name</label>
-            <input type="text" name="name" className="rounded-lg" />
+            <input
+              type="text"
+              name="name"
+              className="rounded-lg dark:bg-gray-700"
+            />
           </div>
           <div className="flex flex-col space-y-2">
             <label htmlFor="privacy">Group privacy</label>
-            <select name="privacy" id="privacy" className="rounded-lg">
+            <select
+              name="privacy"
+              id="privacy"
+              className="rounded-lg dark:bg-gray-700"
+            >
               <option value="public" selected>
                 Public
               </option>
@@ -22,7 +37,7 @@ export default function Create() {
           </div>
 
           <div>
-            <button className="w-full py-3 px-4 bg-blue-500 text-white rounded-lg text-lg">
+            <button className="w-full py-2 px-3 bg-blue-500 text-white rounded-lg text-lg">
               Create
             </button>
           </div>
