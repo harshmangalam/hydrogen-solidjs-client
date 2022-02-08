91b873b5df2e68fadcb2e096f3abfb2ed4875814
Harsh Mangalam <hmangalam@oplglobal.com>
2022-02-08 20:48:20 +0530

    dark profile page

--- src/screens/profile/index.jsx
+++ src/screens/profile/index.jsx
@@ -15,15 +15,16 @@ export default function Profile() {
           <AddCoverPhoto />
         </div>
 
-        <div className="max-w-4xl w-full mx-auto py-2">
-          <ul className="flex items-center space-x-4">
+        <div className="max-w-4xl w-full mx-auto py-3">
+          <hr className="dark:border-gray-700 border-gray-300" />
+          <ul className="flex items-center space-x-4 mt-4">
             <For each={tabs}>
               {(tab) => (
                 <li>
                   <NavLink
                     href={`/${userId}/${tab.href}`}
-                    className="font-medium  hover:bg-gray-100 py-2 px-4 text-gray-600 rounded"
-                    activeClass="border-b-4 border-blue-500 text-blue-500 hover:bg-transparent rounded-none"
+                    className="font-medium  hover:bg-gray-100 py-3 px-4 text-gray-600 rounded dark:text-gray-300 dark:hover:bg-gray-700"
+                    activeClass="border-b-4 border-blue-500 text-blue-500 hover:bg-transparent rounded-none dark:hover:bg-transparent"
                     end={tab.end}
                   >
                     {tab.name}
@@ -34,7 +35,7 @@ export default function Profile() {
           </ul>
         </div>
       </div>
-      <div className="max-w-4xl w-full mx-auto py-4">
+      <div className="max-w-4xl w-full mx-auto py-4 dark:text-white">
         <Outlet />
       </div>
     </div>
