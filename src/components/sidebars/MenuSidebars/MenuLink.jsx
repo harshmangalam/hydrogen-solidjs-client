de1c98fdebe139e8cf8cc469ca8b034e03a21d6e
Harsh Mangalam <hmangalam@oplglobal.com>
2022-03-01 22:54:39 +0530

    home page

--- src/components/sidebars/MenuSidebars/MenuLink.jsx
+++ src/components/sidebars/MenuSidebars/MenuLink.jsx
@@ -5,10 +5,10 @@ export default function MenuLink(props) {
     <li>
       <NavLink
         href={props.href}
-        className="flex items-center space-x-4 py-4 rounded-lg px-4 hover:bg-gray-200 dark:hover:bg-gray-800"
+        className="flex items-center space-x-4 py-3 rounded-lg px-4 hover:bg-gray-200 dark:hover:bg-gray-800"
         activeClass="bg-gray-200 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-800"
       >
-        <span className="text-2xl ">{props.icon()}</span>
+        <span className="text-3xl">{props.icon()}</span>
         <span className="text-md"> {props.name}</span>
       </NavLink>
     </li>
--- src/components/sidebars/MenuSidebars/index.jsx
+++ src/components/sidebars/MenuSidebars/index.jsx
@@ -4,7 +4,7 @@ import { RiWeatherMoonCloudyFill } from "solid-icons/ri";
 import { SiMessenger } from "solid-icons/si";
 
 import MenuLink from "./MenuLink";
-import Avatar from "../../ui/dataDisplay/Avatar";
+import UserAvatar from "../../ui/dataDisplay/UserAvatar";
 import { useAuthState } from "../../../context/auth";
 
 export default function MenuSidebar() {
@@ -13,10 +13,15 @@ export default function MenuSidebar() {
     <div
       className={`h-screen fixed py-14 top-4 lg:block lg:w-3/12 hidden left-0 bg-gray-100 dark:bg-gray-900  px-2 text-gray-800 dark:text-gray-100 hover:overflow-y-scroll custom-scrollbar`}
     >
-      <ul className="flex flex-col space-y-2">
+      <ul className="flex flex-col">
         <MenuLink
-          name={currentUser.firstName}
-          icon={() => <Avatar className="w-6 h-6" />}
+          name={currentUser.firstName + " " + currentUser.lastName}
+          icon={() => (
+            <UserAvatar
+              src={currentUser.profileImage}
+              className="w-8 h-8 rounded-full"
+            />
+          )}
           href={currentUser.id}
         />
         <For each={links}>{(link) => <MenuLink {...link} />}</For>
@@ -29,28 +34,23 @@ const links = [
   {
     name: "Friends",
     href: "/friends",
-    icon: () => <FaSolidUserFriends />,
+    icon: () => <FaSolidUserFriends className="text-rose-500" />,
   },
   {
     name: "Posts",
     href: "/posts",
-    icon: () => <BsFilePost />,
+    icon: () => <BsFilePost  className="text-purple-500" />,
   },
 
   {
     name: "Groups",
     href: "/groups",
-    icon: () => <FaSolidUsers />,
+    icon: () => <FaSolidUsers  className="text-green-500" />,
   },
 
   {
     name: "Weather",
     href: "/weather",
-    icon: () => <RiWeatherMoonCloudyFill />,
-  },
-  {
-    name: "Messenger",
-    href: "/messenger",
-    icon: () => <SiMessenger />,
+    icon: () => <RiWeatherMoonCloudyFill  className="text-blue-500" />,
   },
 ];
