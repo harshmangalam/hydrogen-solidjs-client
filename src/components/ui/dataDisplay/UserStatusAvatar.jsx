33dfb0f4a0bf5770420fcde60cd1ef7acaa7a64d
Harsh Mangalam <hmangalam@oplglobal.com>
2022-03-13 18:47:36 +0530

    user avatar

--- src/components/ui/dataDisplay/UserStatusAvatar.jsx
+++ src/components/ui/dataDisplay/UserStatusAvatar.jsx
@@ -1,21 +1,24 @@
+import { mergeProps } from "solid-js";
+
 export default function UserStatusAvatar(props) {
+  const merged = mergeProps({imgClass:"w-10 h-10"},props)
   return (
-    <div className={`relative ${props.divClass}`}>
+    <div className={`relative ${merged.divClass}`}>
       <img
-        src={props.profileImage}
-        alt={props.firstName}
-        className={`w-10 h-10 rounded-full ${props.imgClass}`}
-        title={props.firstName}
+        src={merged.profileImage}
+        alt={merged.firstName}
+        className={`rounded-full ${merged.imgClass}`}
+        title={merged.firstName}
       />
-      <div className={`absolute -right-1 bottom-0 ${props.statusClass}`}>
+      <div className={`absolute -right-1 bottom-0 ${merged.statusClass}`}>
         <Switch>
-          <Match when={props?.status === "ACTIVE"}>
+          <Match when={merged?.status === "ACTIVE"}>
             <div className="h-4 w-4 bg-green-500 rounded-full border-2 border-white dark:border-black"></div>
           </Match>
-          <Match when={props?.status === "IDLE"}>
+          <Match when={merged?.status === "IDLE"}>
             <div className="h-4 w-4 bg-yellow-500 rounded-full border-2 border-white dark:border-black"></div>
           </Match>
-          <Match when={props?.status === "LOGOUT"}>
+          <Match when={merged?.status === "LOGOUT"}>
             <div className="h-4 w-4 bg-red-500 rounded-full border-2 border-white dark:border-black"></div>
           </Match>
         </Switch>
