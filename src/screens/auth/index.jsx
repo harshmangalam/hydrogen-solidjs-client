69b15fc1e0b17cd4db2a4744cf9b5e49d1a48462
Harsh Mangalam <hmangalam@oplglobal.com>
2022-03-15 09:53:27 +0530

    handle unauth user

--- src/screens/auth/index.jsx
+++ src/screens/auth/index.jsx
@@ -1,7 +1,16 @@
-import { Outlet } from "solid-app-router";
+import { Outlet, useNavigate } from "solid-app-router";
+import { onMount } from "solid-js";
 import DarkLightMode from "../../components/shared/DarkLightMode";
 import Logo from "../../components/shared/Logo";
+import { useAuthState } from "../../context/auth";
 export default function AuthLayout() {
+  const { isAuthenticated } = useAuthState();
+  const navigate = useNavigate();
+  onMount(() => {
+    if (isAuthenticated) {
+      navigate("/");
+    }
+  });
   return (
     <div>
       <header className="bg-white shadow border-b fixed w-full top-0 z-50 dark:bg-gray-800 dark:border-gray-700">
@@ -12,11 +21,12 @@ export default function AuthLayout() {
       </header>
       <div className="py-14 dark:text-white">
         <Outlet />
-       
       </div>
 
       <footer className="bg-white fixed w-full bottom-0 z-50 dark:bg-gray-800 dark:border-gray-700 dark:text-white py-2">
-       <p className="text-center">Hydrogen &copy; {new Date().getFullYear()}</p>
+        <p className="text-center">
+          Hydrogen &copy; {new Date().getFullYear()}
+        </p>
       </footer>
     </div>
   );
