39f2c7cf16b73bdde3f74b655cd2bcde8823569b
Harsh Mangalam <hmangalam@oplglobal.com>
2022-03-24 10:30:18 +0530

    posts bug fix

--- src/screens/posts/FriendsPosts.jsx
+++ src/screens/posts/FriendsPosts.jsx
@@ -1,35 +1,38 @@
-import { createResource, Match, Switch } from "solid-js";
+import { createResource, Match, Show, Switch } from "solid-js";
 import PostCard from "../../components/posts/PostCard";
 import PostCardSkeleton from "../../components/posts/PostCardSkeleton";
+import Empty from "../../components/shared/Empty";
 import Error from "../../components/shared/Error";
 import { fetchFriendsPosts } from "../../services/post.service";
 export default function Friendsposts() {
-  const [response, { refetch }] = createResource(fetchFriendsPosts);
+  const [resource, { refetch }] = createResource(fetchFriendsPosts);
 
   return (
     <Switch>
-      <Match when={response.loading}>
+      <Match when={resource.loading}>
         <PostCardSkeleton />
       </Match>
-      <Match when={response.error}>
+      <Match when={resource.error}>
         <Error name="Error" />
       </Match>
 
-      <Match when={response().data.data.posts.length === 0}>
-        <Error error="empty" name="No Posts" message="No any posts available" />
-      </Match>
-      <Match when={response().data.data.posts}>
-        <div className="max-w-lg mx-auto">
-          <ul className="grid grid-cols-1 gap-4">
-            <For each={response().data.data.posts}>
-              {(post) => (
-                <li>
-                  <PostCard {...post} refetch={refetch} />
-                </li>
-              )}
-            </For>
-          </ul>
-        </div>
+      <Match when={resource()}>
+        <Show
+          when={resource().data.data.posts.length}
+          fallback={<Empty title="No Posts" />}
+        >
+          <div className="max-w-lg mx-auto">
+            <ul className="grid grid-cols-1 gap-4">
+              <For each={resource().data.data.posts}>
+                {(post) => (
+                  <li>
+                    <PostCard {...post} refetch={refetch} />
+                  </li>
+                )}
+              </For>
+            </ul>
+          </div>
+        </Show>
       </Match>
     </Switch>
   );
--- src/screens/posts/Trending.jsx
+++ src/screens/posts/Trending.jsx
@@ -1,39 +1,38 @@
-import { createResource, Match, Switch } from "solid-js";
+import { createResource, Match, Show, Switch } from "solid-js";
 import PostCard from "../../components/posts/PostCard";
 import PostCardSkeleton from "../../components/posts/PostCardSkeleton";
 import Error from "../../components/shared/Error";
+import Empty from "../../components/shared/Empty";
 import { fetchTrendingPosts } from "../../services/post.service";
 export default function TrendingPosts() {
-  const [response, { refetch }] = createResource(fetchTrendingPosts);
+  const [resource, { refetch }] = createResource(fetchTrendingPosts);
 
   return (
     <Switch>
-      <Match when={response.loading}>
+      <Match when={resource.loading}>
         <PostCardSkeleton />
       </Match>
-      <Match when={response.error}>
-        <Error
-          error="server"
-          name={response.error.name}
-          message={response.error.message}
-        />
-      </Match>
-
-      <Match when={response().data.data.posts.length === 0}>
+      <Match when={resource.error}>
         <Error name="Error" />
       </Match>
-      <Match when={response().data.data.posts}>
-        <div className="max-w-lg mx-auto">
-          <ul className="grid grid-cols-1 gap-4">
-            <For each={response().data.data.posts}>
-              {(post) => (
-                <li>
-                  <PostCard {...post} refetch={refetch} />
-                </li>
-              )}
-            </For>
-          </ul>
-        </div>
+
+      <Match when={resource()}>
+        <Show
+          when={resource().data.data.posts.length}
+          fallback={<Empty title="No Posts" />}
+        >
+          <div className="max-w-lg mx-auto">
+            <ul className="grid grid-cols-1 gap-4">
+              <For each={resource().data.data.posts}>
+                {(post) => (
+                  <li>
+                    <PostCard {...post} refetch={refetch} />
+                  </li>
+                )}
+              </For>
+            </ul>
+          </div>
+        </Show>
       </Match>
     </Switch>
   );
