f389edaaf1eb7e2f304284fd3fa32441e5e6f794
Somenath Choudhury <somenathchoudhury35@gmail.com>
2022-10-01 17:45:41 +0530

    commited

--- src/components/posts/PostCard/DeletePost.jsx
+++ src/components/posts/PostCard/DeletePost.jsx
@@ -7,9 +7,10 @@ export default function DeletePost(props) {
   return (
     <div className="relative">
       <button
-        class="p-2 hover:bg-red-100 dark:hover:bg-red-400 dark:text-white text-xl rounded-full text-red-500"
+        class="tooltip last:p-2 hover:bg-red-100 dark:hover:bg-red-400 dark:text-white text-xl rounded-full text-red-500"
         onClick={() => setOpen(true)}
       >
+        <span className="tooltiptext-delete font-medium">Delete Post</span>
         <AiOutlineDelete />
       </button>
 
