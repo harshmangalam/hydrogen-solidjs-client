c8e7982027c5b6b9c6f9ff7512099f3d68cf16b2
harshmangalam <harshdev8218@gmail.com>
2022-10-05 19:29:41 +0530

    added shared delete dialog

---
+++ src/components/shared/DeleteDialog.jsx
@@ -0,0 +1,34 @@
+import { createSignal, Show } from "solid-js";
+import { AiOutlineDelete } from "solid-icons/ai";
+import ConfirmDialog from "../../ui/feedback/ConfirmDialog";
+
+export default function DeleteDialog(props) {
+  const [open, setOpen] = createSignal(false);
+  return (
+    <div className="relative">
+      <button
+        class="p-2 hover:bg-red-100 dark:hover:bg-red-400 dark:text-white text-xl rounded-full text-red-500"
+        onClick={() => setOpen(true)}
+      >
+        <AiOutlineDelete />
+      </button>
+
+      {/* <!-- Dropdown menu --> */}
+
+      <ConfirmDialog
+        open={open()}
+        onClose={() => setOpen(false)}
+        title={props.title}
+        content={props.content}
+        actions={[
+          { name: "No", onClick: () => setOpen(false) },
+          {
+            name: "Yes",
+            onClick: () => props.handleDelete(),
+            variant: "danger",
+          },
+        ]}
+      />
+    </div>
+  );
+}
