bf5cb3317ebfa18a4662d4559387739fd845d52d
harshmangalam <harshdev8218@gmail.com>
2022-10-08 09:04:57 +0530

    added fixes in group card

--- src/components/groups/GroupCard.jsx
+++ src/components/groups/GroupCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from "solid-app-router";
+import UserAvatarTag from "../shared/UserAvatarTag";
 export default function GroupCard(props) {
   return (
     <article className="bg-white dark:bg-gray-800 shadow border dark:border-gray-700 rounded-lg dark:text-white">
@@ -9,26 +10,18 @@ export default function GroupCard(props) {
       />
 
       <div className="py-4 px-4">
-        <Link href={`/groups/${props.id}`} className="hover:underline">
-          <h6 className="font-medium text-lg">{props.name}</h6>
-        </Link>
+        <div className="flex items-center justify-between gap-4">
+          <Link href={`/groups/${props.id}`} className="hover:underline">
+            <h6 className="font-medium text-lg">{props.name}</h6>
+          </Link>
+          <UserAvatarTag {...props.admin} />
+        </div>
         <div className="flex items-center justify-between flex-wrap gap-2">
           <Link
             href={`/groups/${props.id}/members`}
             className="hover:underline"
           >
             {props._count.members} members
-          </Link>
-          <Link
-            href={`/${props.admin.id}`}
-            className="flex items-center gap-1 hover:underline"
-          >
-            <img
-              src={props.admin.profileImage}
-              alt={props.admin.firstName}
-              className="aspect-square object-cover w-4 h-4 rounded-full"
-            />
-            <span>{props.admin.firstName}</span>
           </Link>
         </div>
       </div>
