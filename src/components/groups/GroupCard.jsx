ccc6562e11d8086e2c755b322b9e590fb682b7e8
chankruze <chankruze@gmail.com>
2022-10-08 08:22:59 +0530

    fix: show group admin name on group card
    fixes #67
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/components/groups/GroupCard.jsx
+++ src/components/groups/GroupCard.jsx
@@ -12,9 +12,25 @@ export default function GroupCard(props) {
         <Link href={`/groups/${props.id}`} className="hover:underline">
           <h6 className="font-medium text-lg">{props.name}</h6>
         </Link>
-        <Link href={`/groups/${props.id}/members`} className="hover:underline">
-          {props._count.members} members
-        </Link>
+        <div className="flex items-center justify-between flex-wrap gap-2">
+          <Link
+            href={`/groups/${props.id}/members`}
+            className="hover:underline"
+          >
+            {props._count.members} members
+          </Link>
+          <Link
+            href={`/${props.admin.id}`}
+            className="flex items-center gap-1 hover:underline"
+          >
+            <img
+              src={props.admin.profileImage}
+              alt={props.admin.firstName}
+              className="aspect-square object-cover w-4 h-4 rounded-full"
+            />
+            <span>{props.admin.firstName}</span>
+          </Link>
+        </div>
       </div>
 
       <div className="py-4 px-4">{props.children}</div>
