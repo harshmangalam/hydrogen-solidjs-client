09ab4ac61b43ba1226acc6dbb81a0556a21ace29
Tomas Danko <tomas.danko@student.tuke.sk>
2022-10-23 12:14:12 +0200

    Profile/about - Overview and Contact added

--- src/screens/profile/about/Contact.jsx
+++ src/screens/profile/about/Contact.jsx
@@ -1,7 +1,18 @@
-export default function Contact(){
-    return (
-        <div>
-            Contact
-        </div>
-    )
+import { useAuthState } from "../../../context/auth";
+import { Match, Switch } from "solid-js";
+
+export default function Contact() {
+  const { currentUser } = useAuthState();
+  return (
+    <Switch>
+      <Match when={currentUser}>
+        <ul className="flex gap-0 dark:divide-gray-600 flex-col divide-y-2 max-w-xl mx-auto my-auto">
+          <li className="py-1 flex justify-between items-center">
+            <span className="font-semibold">Email</span>
+            <span>{currentUser.email}</span>
+          </li>
+        </ul>
+      </Match>
+    </Switch>
+  );
 }
--- src/screens/profile/about/Overview.jsx
+++ src/screens/profile/about/Overview.jsx
@@ -1,7 +1,48 @@
-export default function Overview(){
+import { useAuthState } from "../../../context/auth";
+import { Match, Switch } from "solid-js";
+
+export default function Overview() {
+  const { currentUser } = useAuthState();
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const getDateAndTimeFormat = (timestamp) => {
+    const date = new Date(timestamp);
     return (
-        <div>
-            Overview
-        </div>
-    )
+      date.toLocaleDateString().slice(0, 2) +
+      " " +
+      date.toLocaleDateString("default", { month: "short" }) +
+      " " +
+      date.toLocaleDateString().slice(-4)
+    );
+  };
+
+  return (
+    <Switch>
+      <Match when={currentUser}>
+        <ul className="flex gap-0 dark:divide-gray-600 flex-col divide-y-2 max-w-xl mx-auto my-auto">
+          <li className="py-1 flex justify-between items-center">
+            <span className="font-semibold">First Name</span>
+            <span>{currentUser.firstName}</span>
+          </li>
+          <li className="py-1 flex justify-between items-center">
+            <span className="font-semibold">Last Name</span>
+            <span>{currentUser.lastName}</span>
+          </li>
+          <li className="py-1 flex justify-between items-center">
+            <span className="font-semibold">Gender</span>
+            <span>
+              {capitalizeFirstLetter(currentUser.gender.toLowerCase())}
+            </span>
+          </li>
+          <li className="py-1 flex justify-between items-center">
+            <span className="font-semibold">Registered</span>
+            <span>{getDateAndTimeFormat(currentUser.createdAt)}</span>
+          </li>
+        </ul>
+      </Match>
+    </Switch>
+  );
 }
